f1b83861d0c7f70ca91c3dad44dd6fda35a33c9d
DorinHornea <imotim@gmail.com>
2014-02-27 20:52:16 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>My Smart City</title>
+<link rel="stylesheet" href="styles.css" type="text/css" />
+
+</head>
+<body>
+<div id="container">
+	<div id="header">
+    	<h1><a href="mysmartcity.ri">MySmartCity</a></h1>
+        <h2>O comunitate deschisa pentru o societate transparenta.</h2>
+        <div class="clear"></div>
+    </div>
+    
+	<?php include 'menu.php'; ?>
+	
+    <div id="body">
+		<div id="content">
+			<h2>Intalnire cu reprezentantii <strong>Primariei Timisoara</strong>. </h2>
+            </br>
+			<p>Azi, de ziua indragostitilor, am avut o intalnire foarte importanta si constructiva cu reprezentantii Primariei Timisoara.</p>
+			<h4>Participanti:</h4>
+			<ul class="styledlist">
+				<li>- Primaria Timisoara - Dan Diaconu, Ovidiu Simonetti, Vesna Struta, Alin Stoica;</li>
+				<li>- Smart City TM - Dan si Bogdan;</li>
+            </ul>
+			</br>
+            <h4>S-au discutat si hotarat urmatoarele:</h4>
+
+
+            <ul class="styledlist">
+                <li>necesitatea expunerii unor seturi de date deschise, si in contextul recentului interes manifestat de alte orase (Cluj, Bucuresti, Sibiu); datele ar trebui sa fie expuse pana in data de 22.02 (data hackaton-ului Open Data, tinut in Bucuresti);</li>
+                <li>s-a stabilit expunerea primului set de date deschise (nomenclatorul strazilor), de catre d-na Vesna; pentru a putea upload-a un prim set de date deschise, d-na Vesna va solicita un cont pe portalul <a href="http://data.gov.ro" title="data.gov.ro">data.gov.ro</a>. Dan o va ajuta cu urgentarea crearii contului;</li>
+                <li>s-a discutat despre alte posibile seturi de date, care (poate) se vor putea expune pana pe data de 22. Aceste seturi de date vor fi analizate de reprezentantii Primariei si, in masura in care se pot obtine in timp util, vor fi expuse pe <a href="http://data.gov.ro" title="data.gov.ro">data.gov.ro</a>.</li>
+                <li>s-a analizat referatul privind acordul de voluntariat dintre <strong>Primaria TM</strong> si <strong>comunitatea SmartCity</strong>; acesta urmeaza sa fie revizuit de noi (prin Denis) si sa fie rediscutat si semnat in saptamanile ce urmeaza;</li>
+				<li>de saptamana viitoare vom continua discutiile organizatorice si tehnice pentru identificarea si expunerea altor seturi de date;</li>
+				<li>noi (comunitatea SmartCity) vom trimite un header de tabel ce va fi folosit de reprezentantii Primariei pentru a solicita date de la institutiile arondate Primariei. Acest header de tabel va fi util pentru a defini un set minim de informatii care ar trebui sa fie furnizate;</li>
+            </ul>
+
+            
+			<h3>Buy unbranded</h3>
+
+
+
+            <p>Lorem ipsum dolor sit amet, ne cum debitis antiopam, mel an erat putent, vocent quaeque at vis. Usu unum tation vidisse te. Qui te alia fierent, ea duo ubique aperiri constituto. Simul abhorreant ei usu, ex pri ferri adversarium.Ut nibh debitis vix. Ea iudico regione nec. Veri perpetua ius in. Eu sed errem nominati tractatos, qui luptatum legendos sapientem in. Te per dico dissentiunt, sit ut urbanitas voluptatibus. Deserunt appellantur te duo, purto nonumes veritus te mei. At has tollit ornatus invenire, eu minim noluisse convenire cum. Id falli zril eam, no enim euripidis laboramus mel. Porro aperiri senserit cu usu, et fuisset aliquando urbanitas mel. Eam te omnesque convenire, est falli soleat temporibus ex, integre labores eleifend te eam. Percipit insolens ei ius, option dissentiet ei sit. Dicam vocent ancillae his ei, eu duo electram incorrupte, erat mutat usu id. Offendit instructior ea mea, est te stet dicat partiendo. Laoreet veritus rationibus in vim.</p>
+
+<h3>Lorem lipsum</h3>
+
+<p>Morbi fermentum condimentum felis, commodo vestibulum sem mattis sed. Aliquam magna ante, mollis vitae tincidunt in, malesuada vitae turpis. Sed aliquam libero ut velit bibendum consectetur. Quisque sagittis, est in laoreet semper, enim dui consequat felis, faucibus ornare urna velit nec leo. Maecenas condimentum velit vitae est lobortis fermentum. In tristique sem vitae metus ornare luctus tempus nisl volutpat. Integer et est id nisi tempus pharetra sagittis et libero.</p>
+
+
+        </div>
+        
+<?php include 'right.php'; ?>
+		
+    	<div class="clear"></div>
+
+<?php include 'footer.php'; ?>		
+		
+</div>
+</body>
+</html>
